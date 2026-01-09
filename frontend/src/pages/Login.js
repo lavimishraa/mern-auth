f51c65764a44764fd7bd603b4e173dfea10d8240
Lavi Mishra <151001308+LaviMishraa@users.noreply.github.com>
2026-01-09 16:31:09 +0530

    Login.js

--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -20,7 +20,7 @@ function Login() {
 
     try {
       const res = await axios.post(
-        "http://localhost:5000/api/auth/login",
+        "https://mern-auth-backend.onrender.com/api/auth/login",
         formData
       );
 
